7567996e0a7b5d78f9259fe6acaea58af6427a77
arnab <arnab47stars@gmail.com>
2020-06-18 22:34:58 +0530

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import * as Battery from "expo-battery";
+import { Audio } from "expo-av";
 
-export default function App() {
+const App = () => {
+  const [level, setLevel] = useState(0);
+
+  const getLevel = async () => {
+    console.log("A: " + (await Battery.getBatteryLevelAsync()));
+    let batteryLevel = Math.round((await Battery.getBatteryLevelAsync()) * 100);
+    setLevel(batteryLevel);
+    Battery.addBatteryLevelListener(({ batteryLevel }) => {
+      batteryLevel = Math.round(batteryLevel * 100);
+      setLevel(batteryLevel);
+      if (batteryLevel == 22) {
+        playAlert();
+      }
+      console.log("B: " + batteryLevel);
+    });
+  };
+
+  const playAlert = async () => {
+    const soundObject = new Audio.Sound();
+    try {
+      await soundObject.loadAsync(require("./assets/alert.mp3"));
+      await soundObject.playAsync();
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    // Anything in here is fired on component mount.
+    // componentDidMount
+    getLevel();
+    return () => {
+      // Anything in here is fired on component unmount.
+      // componentWillUnmount
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{level}%</Text>
+      <Text style={{ color: "#adb5bd", position: "absolute", bottom: 50 }}>
+        v1.0.0
+      </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#21252a",
+  },
+  text: {
+    color: "#adb5bd",
+    fontSize: 50,
+    fontWeight: "bold",
   },
 });
+
+export default App;
